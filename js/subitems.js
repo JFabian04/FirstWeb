f5fc5454c387a300f23ca389f121cad7cf62818a
JFabian04 <jerlinson_ramos@spy.sena.edu.co>
2022-11-03 11:36:08 -0500

    JS

---
+++ js/subitems.js
@@ -0,0 +1,21 @@
+let menuInicio = document.getElementById("inicio");
+menuInicio.addEventListener("click", function () {
+    alert("Welcome!")
+});
+
+let menuNosotros = document.getElementById("nosotros");
+menuNosotros.addEventListener("click", function () {
+    alert("Equipo de trabajo!")
+});
+
+let menuContactenos = document.getElementById("contactenos");
+menuContactenos.addEventListener("click", function () {
+    alert("Correo Electronico")
+});
+
+let menuAyuda = document.getElementById("ayuda");
+menuAyuda.addEventListener("click", function(){
+    alert("Help Us!")
+})
+
+
