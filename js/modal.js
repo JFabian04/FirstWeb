84c3098e5b1998e0872c3a4926014bc4f32797fd
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-04-26 21:09:48 -0500

    Snekaers Almost Complete

--- js/modal.js
+++ js/modal.js
@@ -103,11 +103,21 @@ let bodyBoxTwo = document.getElementById("bodyBox")
 let coverMain = document.getElementById("coverMain")
 let rowCircle = document.querySelectorAll(".circle-row")
 let right = document.getElementById("right")
+let exit = document.getElementById("ex")
 
 main.addEventListener("click", function () {
     mainModal.classList.add("main-modal-vis")
     bodyBoxTwo.classList.add("body-vis")
     coverMain.classList.add("cover-main")
+    exit.classList.add("visible")
+
+    coverMain.addEventListener("click", function () {
+        bodyBoxTwo.classList.remove("cover-main")
+        mainModal.classList.remove("main-modal-vis")
+        coverMain.classList.remove("cover-main")
+        exit.classList.remove("visible")
+
+    })
     if (document.querySelector(".main-modal-vis") != null) {
         for (let i = 0; i < thubnail.length; i++) {
             thubnail[i].addEventListener("click", function () {
@@ -131,45 +141,128 @@ main.addEventListener("click", function () {
                 }
             })
 
+            let cont = 0
+            let contSecond = 4
+            let reverse = 3
+            let reverseSecond = 7
+            // for (let index = 0; index < thubnail.length; index++) {
+            for (let row = 0; row < rowCircle.length; row++) {
+                rowCircle[row].addEventListener("click", function () {
+                    border[7].classList.remove("bord")
+                    border[6].classList.remove("bord")
+                    border[5].classList.remove("bord")
+                    border[4].classList.remove("bord")
+                    border[3].classList.remove("bord")
+                    border[2].classList.remove("bord")
+                    border[1].classList.remove("bord")
+                    border[0].classList.remove("bord")
+                    opactity[9].classList.remove("img-vis")
+                    opactity[8].classList.remove("img-vis")
+                    opactity[7].classList.remove("img-vis")
+                    opactity[6].classList.remove("img-vis")
+                    opactity[5].classList.remove("img-vis")
+                    opactity[4].classList.remove("img-vis")
+                    opactity[3].classList.remove("img-vis")
+                    opactity[2].classList.remove("img-vis")
+                    opactity[1].classList.remove("img-vis")
+                    opactity[0].classList.remove("img-vis")
+
+                    console.log("CONT " + cont)
+                    // border[7].classList.remove("bord")
+                    // border[0].classList.remove("bord")
+                    // if(cont!= 0){
+                    //     reverse = cont
+                    //     console.log("AAA " + reverse)
+                    // }
+                    if (row == 1) {
+                        for (let im = 0; im < mainImg.length; im++) {
+                            mainImg[im].setAttribute("src", arr[cont])
+                        }
+                        console.log("CONTSECOND " + contSecond)
+                        // border[cont].classList.add("bord")
+                        // border[cont + 4].classList.add("bord")
+                        // border[cont + 3].classList.remove("bord")
+                        // border[cont + 2].classList.remove("bord")
+                        border[cont].classList.add("bord")
+                        opactity[cont + 1].classList.add("img-vis")
+                        border[contSecond].classList.add("bord")
+                        opactity[contSecond + 2].classList.add("img-vis")
+                        cont = cont + 1
+
+                        contSecond = contSecond + 1
+                        if (cont == 4) {
+                            cont = 0
+                            contSecond = 4
+
+                        }
+
+
+                        // if( cont == 2){
+                        //     border[0].classList.remove("bord")
+                        // }
+                    }
+
+                    if (row == 0) {
+                        for (let im = 0; im < mainImg.length; im++) {
+                            mainImg[im].setAttribute("src", arr[reverse])
+                        }
+                        console.log("RE " + reverse)
+                        border[reverse].classList.add("bord")
+                        border[reverseSecond].classList.add("bord")
+                        opactity[reverse + 1].classList.add("img-vis")
+                        opactity[reverseSecond + 2].classList.add("img-vis")
+                        // border[reverseSecond].classList.add("bord")
+                        // border[reverseSecond].classList.remove("bord")
+                        // if (reverse == reverse-1) {
+                        //     console.log("REMOVE " + (reverse+1))
+                        //     // border[(reverse + 1)].classList.remove("bord")
+
+                        // }
+                        reverse = reverse - 1
+                        reverseSecond = reverseSecond - 1
+
+                        if (reverseSecond == 3) {
+                            reverse = 3
+                            reverseSecond = 7
+                        }
+                    }
+
+
+                })
+                // }
+            }
+
         }
     }
+    // for (let index = 0; index < 2; index++) {
+    //     rowCircle[index].addEventListener("click", function () {
 
+    //         if (index == 1) {
+    //             let cont = 0
+    //             console.log("INDEZ " + index)
+    //             console.log("CONT " + cont)
 
-    for (let index = 0; index <= 1; index++) {
-        rowCircle[index].addEventListener("click", function () {
-            if (index == 1) {
-                // console.log("INDEZ " + index)
-                let cont = 0
-                for (let i = 0; i <= 4; i++) {
-                    cont = cont + i
-                    console.log("I "  + i)
-                    console.log("CONTADOR "  + cont)
-                    if(cont == i+1){
-                        console.log("ITOW" + (i+1))
-                        console.log("CONTADORTWO" + cont)
-                    }
-                }
-            }
-            // for (let ind = 3; ind < index + 1; ind++) {
-            //     if (index == ind + 4) {
-            //         // border[ind].classList.add("bord")
-            //         console.log("RIGHT " + (i - 3))
-            //         border[index - 3].classList.add("bord")
-            //         // opactity[ind - 3].classList.add("img-vis")
-            //         // opactity[i + 2].classList.add("img-vis")
-            //         for (let im = 0; im < mainImg.length; im++) {
-            //             mainImg[im].setAttribute("src", arr[index - 4])
-            //         }
-            //     } else {
-            //         for (let ind = 7; ind >= 0; ind--) {
-            //             border[ind].classList.remove("bord")
-            //             opactity[ind + 1].classList.remove("img-vis")
-            //             opactity[ind + 2].classList.remove("img-vis")
-            //         }
-            //     }
-            // }
-        })
-    }
+    //             for (let ind = 0; ind < cont +1; ind++) {
+    //                 console.log("IND-MAIN " + ind)
+    //                 for (let i = 0; i < ind + 1; i++) {
+
+    //                     console.log("IND " + ind)
+    //                     console.log("I " + i)
+    //                     if (ind == i) {
+    //                         console.log("IGUAL")
+    //                         console.log("IND-T " + ind)
+    //                         console.log("I-T " + i)
+
+    //                     } else {
+    //                         console.log("No igual")
+    //                     }
+    //                 }
+    //             }
+    //             cont = cont + 1
+    //         }
+
+    //     })
+    // }
 })
 if (document.querySelector(".main-modal-vis") == null) {
     for (let i = 0; i <= 4; i++) {
