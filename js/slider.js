d7323057988c7212495a25552eb2e252cbc2861b
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-05-11 11:35:49 -0500

    slider incomplete

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,6 @@
 let right = document.getElementById("right");
+let left = document.getElementById("back")
+
 let imgThree = document.getElementById("imgThree");
 let imgTwo = document.getElementById("imgTwo");
 let imgOne = document.getElementById("imgOne")
@@ -8,94 +10,128 @@ let imgContent = document.querySelector(".img-content");
 let icon = document.querySelectorAll(".icon");
 let img = document.querySelectorAll(".img");
 
+
 let cont = 0;
-let ind = -1;
+let ind = 0;
 
 for (let i = 0; i < icon.length; i++) {
-    console.log("MAIN " + i)
     icon[i].addEventListener("click", function () {
-        console.log(ind)
-        console.log("btn " + i)
-        // for (let index = 0; index <  i + 1; index++) {
-            let r = 0
-            // console.log("INDEX " +index)
-            if (i == 0) {
-                ind = ind + 1
-                console.log("CONTONE " + (cont + 1))
-                if (ind == 0) {
-                    imgContent.classList.add("img-content-left")
-                }
-                if (ind == 1) {
-                    imgContent.classList.add("img-content-leftt")
-                    ind = -1
-
-                }
-                // if (ind == 2) {
-                //     imgContent.classList.add("img-content-reverse")
-                //     ind = -1
-                    
-                // }
-            // }
-            // cont = cont + 1
-        }
-        // console.log("CERO " + i)
-        if (i == 1) {
-
-            // if (cont == 0) {
-            //     imgContent.classList.add("img-content-rigth")
-            // }
+        if (i == 0) {
+            if (cont == 0) {
+                imgContent.classList.add("img-content-left")
+                imgContent.classList.remove("img-content-reverse")
+                cont = cont + 1
+                console.log("R ONE " + cont)
+                return cont;
+            }
             if (cont == 1) {
-                imgContent.classList.add("img-content-rightt")
+                imgContent.classList.add("img-content-leftt")
+                imgContent.classList.remove("img-content-left")
+                cont = cont + 1
+                console.log("R TWO " + cont)
+
+                return cont;
+
             }
             if (cont == 2) {
+                imgContent.classList.remove("img-content-leftt")
                 imgContent.classList.add("img-content-reverse")
+                cont = 0;
+                console.log("R Tre " + cont)
+                // return;
             }
+        }
 
-            cont = cont + 1
+        if (i == 1) {
+            if (cont > 0) {
+                ind = cont
+                console.log("condi " + ind)
+                if (ind == 1) {
+                    imgContent.classList.add("img-content-reverse")
+                    // imgContent.classList.remove("img-content-left")
+                    // imgContent.classList.remove("img-content-right")
+                    console.log("L TWO " + ind)
+                    ind = ind + 1
+                    return ind;
+    
+                }
+                return
+            }
+            if (ind == 0) {
+                imgContent.classList.add("img-content-right")
+                imgContent.classList.remove("img-content-reverse")
+                
+                ind = ind + 1
+                console.log("L ONE " + ind)
+                return ind;
+            }
+            if (ind == 1) {
+                imgContent.classList.add("img-content-rightt")
+                // imgContent.classList.remove("img-content-left")
+                imgContent.classList.remove("img-content-right")
+                console.log("L TWO " + ind)
+                ind = ind + 1
+                return ind;
+
+            }
+            if (ind == 2) {
+                imgContent.classList.remove("img-content-rightt")
+                imgContent.classList.add("img-content-reverse") 
+                ind = 0;
+                console.log("L Threr " + ind)
+                return;
+            }
         }
 
     })
 }
 
-// let rgt = true;
-
-//     right.addEventListener("click", function () {
-//         if (rgt == true) {
-//             ind = ind + 1
-
-//             if (ind == 0) {
-//                 imgContent.classList.add("img-content-left")
-//             }
-//             if (ind == 1) {
-//                 imgContent.classList.add("img-content-leftt")
-//             }
-//             if (ind == 2) {
-//                 imgContent.classList.add("img-content-reverse")
-//                 ind = -1
-
-//             }
-
-//         }
-//     })
-
-
-
-
 // right.addEventListener("click", function () {
-//     console.log("CONT " + cont)
-//     console.log("IND " + ind)
-//     if (ind == cont) {
+//     if (cont == 0) {
 //         imgContent.classList.add("img-content-left")
-//         ind = ind + 1
-//         cont = cont + 1;
-//         console.log("CONT " + cont)
-//         console.log("IND " + ind)
+//         imgContent.classList.remove("img-content-reverse")
+//         cont = cont + 1
+//         console.log("R ONE: " + cont)
+//         return cont;
 //     }
-//     if (ind == cont) {
-//         imgContent.classList.add(".img-content-leftt")
-//         ind = ind + 1
-//         cont = cont + 1;
-//         console.log("CONT " + cont)
-//         console.log("IND " + ind)
+//     if (cont == 1) {
+//         imgContent.classList.add("img-content-leftt")
+//         imgContent.classList.remove("img-content-left")
+//         cont = cont + 1
+//         console.log("R TWO " + cont)
+//         return  cont;
+//     }
+//     if (cont == 2) {
+//         imgContent.classList.remove("img-content-leftt")
+//         imgContent.classList.add("img-content-reverse")
+//         cont = 0;
+//         console.log("R Tre " + cont)
+//         return;
 //     }
 // })
+
+// left.addEventListener("click", function(){
+//     if (ind == 0) {
+//         imgContent.classList.add("img-content-right")
+//         imgContent.classList.remove("img-content-reverse")
+//         ind = ind + 1
+//         console.log("L ONE " + ind)
+//         return ind;
+//     }
+//     if (ind == 1) {
+//         imgContent.classList.add("img-content-rightt")
+//         imgContent.classList.remove("img-content-right")
+//         ind = ind + 1
+//         console.log("L TWO " + ind)
+//         return ind;
+    
+//     }
+//     if (ind == 2) {
+//         imgContent.classList.remove("img-content-rightt")
+//         imgContent.classList.add("img-content-reverse") 
+//         ind = 0;
+//         console.log("L Threr " + ind)
+//         return;
+//     }
+// })
+
