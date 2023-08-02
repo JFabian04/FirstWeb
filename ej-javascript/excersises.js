e119a4a290dc6e6b6be4d28d7cfc6957de2ccfec
4Nunez4 <jorgeenrrique123456789@gmail.com>
2023-08-02 16:48:31 -0500

    ejs

---
+++ ej-javascript/excersises.js
@@ -0,0 +1,10 @@
+/* USE ESTRIC OF JS */
+// "use strict"
+
+// noStrict = "Variable no puede ser usuada si no se inicializa";
+// alert(noStric);
+
+
+//
+let text = "2";
+console.log(Number(text) +1)
