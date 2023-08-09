6b52381d954efc4ab4fb83e46569e625a0bfef24
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-08-09 09:02:17 -0500

    searcher homecenter si OK

--- js/seacher.js
+++ js/seacher.js
@@ -1,18 +1,24 @@
 let searcher = document.getElementById("inputMobile");
 let inputSearch = document.getElementById("inputSearchMobile");
 let magnifier = document.getElementById("magnifSearch");
+let magnifIcon = document.getElementById("magnifSearch");
 let exit = document.getElementById("exitSearch");
 
 magnifier.addEventListener("click", function () {
-    searcher.classList.add("container-input-mobile");
     searcher.classList.remove("container-input-mobile-none");
-    // alert("A")
+    searcher.classList.add("container-input-mobile");
+    inputSearch.classList.remove("input-mobile-none");
+    inputSearch.classList.add("input-mobile");
     setTimeout(() => {
-        // searcher.classList.add("transition");
+        searcher.classList.add("transition");
         inputSearch.classList.add("transition");
-    }, "2000")
+    }, "1")
+    // inputSearch.style.transition = "all 1s ease"; 
 })
 exit.addEventListener("click", function () {
     searcher.classList.remove("container-input-mobile");
     searcher.classList.add("container-input-mobile-none");
+    searcher.classList.remove("transition");
+    inputSearch.classList.remove("transition");
+
 })
