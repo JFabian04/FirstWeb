942d89451ace2a48e2bcd1133e778e31e9696b5c
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-08-29 18:25:13 -0500

    chat ana homcenter OK

--- js/seacher.js
+++ js/seacher.js
@@ -31,7 +31,9 @@ let containerChat = document.getElementById("containerChat");
 let sliderChat = document.getElementById("sliderChat");
 let message = document.getElementById("message");
 let header = document.getElementById("header");
+let imgHeader = document.getElementById("imgHeader");
 let scrollContainer = document.getElementById("scrollContainer");
+
 
 chatShowBtn.addEventListener("click", function () {
     if (!chatShowBtn.classList.contains("btn-chat-hidden")) {
@@ -45,19 +47,19 @@ chatShowBtn.addEventListener("click", function () {
         containerChat.style.opacity = "1";
         containerChat.style.transition = "height 0.4s";
         containerChat.style.height = "400px";
-        sliderChat.style.height = "115px";
+        sliderChat.style.height = "145px";
         sliderChat.style.opacity = "1";
         sliderChat.style.transition = "all 0.5s";
         message.style.opacity = "1";
         message.style.transition = "all 0.2s";
-        header.style.height = "40px";
-
+        header.style.height = "50px";
+        imgHeader.style.height = "40px";
 
     } else {
         chatShowBtn.classList.remove("btn-chat-hidden");
         for (let i = 0; i < buttonContent.length; i++) {
             buttonContent[i].style.opacity = "1";
-            buttonContent[i].style.transition = "opacity 1.1s";
+            buttonContent[i].style.transition = "opacity 0.5s";
         }
         containerChat.style.height = "0px";
         containerChat.style.transition = "height 0.5s";
@@ -69,63 +71,105 @@ chatShowBtn.addEventListener("click", function () {
             message.style.opacity = "0";
             message.style.transition = "all 0.2s";
             header.style.height = "0px";
+            imgHeader.style.height = "0px";
 
         }, 100)
         sliderChat.style.height = "0px"
         sliderChat.style.opacity = "0";
         sliderChat.style.transition = "all 0.2s";
-
+        // slider.style.marginLeft = "-" + 0 + "px";
     }
-    //SLIDER CHAT
-    let rowRight = document.getElementById("rowRight");
-    let rowLeft = document.getElementById("rowLeft");
-    let slider = document.getElementById("conatinSlider");
-    let contRight = 0;
-    let contLeft = 0;
-    let marginSize = 295;
-    let marg = -295;
-    if (contLeft == 0) {
-        rowLeft.style.opacity = "0";
+})
+//SLIDER CHAT
+let contPixels = 0;
+let rows = document.querySelectorAll(".rows");
+let slider = document.getElementById("conatinSlider");
+for (let i = 0; i < rows.length; i++) {
+    if (contPixels == 0) {
+        rows[1].style.visibility = "hidden";
     }
-    rowRight.addEventListener("click", function () {
-        rowLeft.style.opacity = "1";
-        contRight = contRight + 1;
+    rows[i].addEventListener("click", function () {
+        if (i == 0) {
+            //Contador menor a 590px (resultado de la multiplicacion)
+            if (contPixels < 295 * rows.length) {
+                contPixels = contPixels + 295;
+                slider.style.marginLeft = "-" + contPixels + "px";
+                console.log(contPixels, "A")
 
-        if (contRight == 2) {
-            marg = -295
-            contLeft = 0;
-        }
-        if (contRight > 1) {
-            marginSize = 295
-            rowRight.style.opacity = "0";
-            marginSize = marginSize * contRight;
-        }
-        slider.style.marginLeft = "-" + marginSize + "px";
-        // slider.style.transition = "margin-left 0.7s";
+                //Hide the rows
+                if (contPixels == 295 * rows.length) {
+                    rows[0].style.visibility = "hidden";
+                } else {
+                    rows[1].style.visibility = "visible";
+                }
+            }
+        } else {
+            if (contPixels > 0) {
+                contPixels = contPixels - 295;
+                slider.style.marginLeft = "-" + contPixels + "px";
 
-    })
-    rowLeft.addEventListener("click", function () {
-        rowRight.style.opacity = "1";
-        contLeft = contLeft + 1;
-        if (contRight == 1 && contLeft == 1) {
-            marg = 0;
-            contRight = 0;
-            rowLeft.style.opacity = "0";
-        }
-        if (contLeft >= 1) {
-            marginSize = 197.5;
-            if (contLeft == 2) {
-                marg = 0;
-                contRight = 0;
-                marginSize = 295;
-                rowLeft.style.opacity = "0";
+                //Hide the rows
+                if (contPixels == 0) {
+                    rows[1].style.visibility = "hidden";
+                } else {
+                    rows[0].style.visibility = "visible";
+                }
             }
         }
-        if (contLeft > 1) {
-            contLeft = 0
-        }
-        slider.style.marginLeft = marg + "px";
-        contRight = 0;
-        marginSize = 295;
     })
-})
+}
+
+// let rowRight = document.getElementById("rowRight");
+// let rowLeft = document.getElementById("rowLeft");
+
+
+// let contRight = 0;
+// let contLeft = 0;
+// let marginSize = 295;
+// let marg = -295;
+
+// if (contLeft == 0) {
+//     rowLeft.style.opacity = "0";
+// }
+// rowRight.addEventListener("click", function () {
+//     rowLeft.style.opacity = "1";
+//     contRight = contRight + 1;
+
+//     if (contRight == 2) {
+//         marg = -295
+//         contLeft = 0;
+//     }
+//     if (contRight > 1) {
+//         marginSize = 295
+//         rowRight.style.opacity = "0";
+//         marginSize = marginSize * contRight;
+//     }
+//     slider.style.marginLeft = "-" + marginSize + "px";
+//     // slider.style.transition = "margin-left 0.7s";
+
+// })
+// rowLeft.addEventListener("click", function () {
+//     rowRight.style.opacity = "1";
+//     contLeft = contLeft + 1;
+//     if (contRight == 1 && contLeft == 1) {
+//         marg = 0;
+//         contRight = 0;
+//         rowLeft.style.opacity = "0";
+//     }
+//     if (contLeft >= 1) {
+//         marginSize = 197.5;
+//         if (contLeft == 2) {
+//             marg = 0;
+//             contRight = 0;
+//             marginSize = 295;
+//             rowLeft.style.opacity = "0";
+//         }
+//     }
+//     if (contLeft > 1) {
+//         contLeft = 0
+//     }
+//     slider.style.marginLeft = marg + "px";
+//     contRight = 0;
+//     marginSize = 295;
+// })
+
