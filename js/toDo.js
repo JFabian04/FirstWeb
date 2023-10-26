b4d93149812c20b40e3939b90d1a4e88b9761951
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-10-25 23:14:11 -0500

    toDo 95%

--- js/toDo.js
+++ js/toDo.js
@@ -10,8 +10,11 @@ const section = document.querySelectorAll('.section')
 
 let cont = 0
 inputCreate.addEventListener('keyup', function (event) {
-    if (event.key == 'Enter') {
+    if (event.key == 'Enter' && inputCreate.value != '') {
         cont++;
+        section.forEach((otherElement) => {
+            otherElement.style.color = otherElement.dataset.originalColor;
+        });
 
         let todoClone = containerTodo.cloneNode(true); //Clon de lo que se va a crear
         let titleTask = todoClone.querySelector('#titleTodo'); //Elemento que recibe el texto
@@ -42,21 +45,37 @@ inputCreate.addEventListener('keyup', function (event) {
         itemsLeft.innerText = cont;
 
         // Focus a item seleccionado del footer/seccion
-        section.forEach((item) => {
+        section.forEach((item, i) => {
             item.dataset.originalColor = item.style.color || getComputedStyle(item).color;
             item.addEventListener('click', () => {
-                section.forEach((otherElement) => {
-                    otherElement.style.color = otherElement.dataset.originalColor;
-                });
-                if (controlTheme) {
-                    item.style.color = "rgb(212, 17, 43)";
-                } else {
-                    item.style.color = "white";
-                }
+                colorItem(item, i);
             });
         });
+    } else if (event.key == 'Enter' && inputCreate.value == '') {
+        Swal.fire(
+            '',
+            'No puedes asignar una tarea vacia.'
+          )
     }
 })
+
+let position = 0;;
+let clickSection = false;
+
+function colorItem(item, i) {
+    section.forEach((otherElement) => {
+        otherElement.style.color = otherElement.dataset.originalColor;
+    });
+    if (controlTheme) {
+        position = i
+        item.style.color = "rgb(212, 17, 43)"; //Modo claro
+        clickSection = true;
+    } else {
+        position = i
+        item.style.color = "white"; //Modo Socuro
+        clickSection = true;
+    }
+}
 
 const completed = document.getElementById('completed');
 completed.addEventListener('click', function () {
@@ -108,7 +127,7 @@ body.dataset.originalColor = window.getComputedStyle(body).backgroundColor;
 const containerItems = document.querySelectorAll('.container-items');
 containerItemOriginal = window.getComputedStyle(containerItems[0]).backgroundColor;
 
-const textItem = document.querySelectorAll('.title-todo');
+const textItem = containerTodo.querySelectorAll('.title-todo');
 textItemOriginal = window.getComputedStyle(textItem[0]).color;
 
 sunIcon.addEventListener('click', function () {
@@ -118,21 +137,26 @@ sunIcon.addEventListener('click', function () {
     sunIcon.style.zIndex = "-1";
 
     // Color de border y texto
-    containerTodo.classList.add('light-mode')
-    textItem.forEach(item => {
-        item.style.color = "red"
-    })
-    
+    // containerTodo.classList.add('light-mode')
+    // textItem.forEach(item => {
+    //     item.style.color = "red"
+    // })
+
+    if (clickSection) {
+        section[position].style.color = "red";
+    }
+
     for (let i = 0; i < containerItems.length; i++) {
         containerItems[i].style.backgroundColor = "gainsboro"
     }
     containerImg.style.backgroundImage = 'url(../img/todo_mentor/bg-desktop-light.jpg)';
     body.style.backgroundColor = "whitesmoke"
 
-
+    inputCreate.style.caretColor = 'black';
+    inputCreate.style.color = 'black';
     controlTheme = true;
-
 })
+
 moonIcon.addEventListener('click', function () {
     sunIcon.style.opacity = "1";
     moonIcon.style.opacity = "0";
@@ -143,16 +167,24 @@ moonIcon.addEventListener('click', function () {
     }
 
     // Color de border y texto
-    containerTodo.classList.add('light-mode')
-    textItem.forEach(item => {
-        item.style.color = "black"
-    })
-    containerTodo.classList.add('light-mode')
+    // containerTodo.classList.add('light-mode')
+    // textItem.forEach(item => {
+    //     item.style.color = "black"
+    // })
+    if (clickSection) {
+        section[position].style.color = "white";
+    }
+
+    // containerTodo.classList.add('light-mode')
     containerImg.style.backgroundImage = 'url(../img/todo_mentor/header-web.jpg)';
     body.style.backgroundColor = body.dataset.originalColor;
 
-    textItem.forEach(item => {
-        item.style.color = textItemOriginal
-    })
+    // textItem.forEach(item => {
+    //     item.style.color = textItemOriginal
+    // })
+
+    inputCreate.style.caretColor = 'white';
+    inputCreate.style.color = 'rgb(154, 155, 175)';
+
     controlTheme = false;
 })
