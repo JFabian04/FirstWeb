e36f8ab7d898f9b20606e14a6120996bd5f35000
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-11-08 22:15:10 -0500

    game ok

--- js/game.js
+++ js/game.js
@@ -1,105 +1,126 @@
 const jackBlack = document.getElementById('jackBlack');
 const ground = document.getElementById('ground');
-
+const montain = document.getElementById('montain');
 
 let position = 0;
 let cont = 0;
 let margin = 0.6;
 let max = false;
-let limit = 190;
+let limit = 100;
+let montainEnd = 26
 
 let timeSet
 let timeToSleep = 3000;
+let interval;
+let timePress = 15;
+let intervalX;
 
-let rightPress = false;
-let upPress = false;
+let pressKeys = {};
+let keyPressed = 0;
 
-window.addEventListener('keyup', function () {
-    jackBlack.style.backgroundImage = "url('./img/game/Parada-original.webp')"
-    clearTimeout(timeSet);
-    timeWaitJack(timeToSleep)
-    rightPress = false;
-    // position = position - 100;
-})
 
 window.addEventListener('keydown', function (event) {
+
     if (event.key == 'ArrowRight') {
-        rightPress = true;
-        cont++
-        position = cont * margin;
-        jackBlack.style.transform = 'scaleX(1)';
-        if (position < limit) {
-            // console.log("GR: ", position);
-            ground.style.marginLeft = '-' + position + 'vw';
-            // max = true;
-        } else {
-            position = position - (limit)
-            // console.log("JACK: ", position);
-            jackBlack.style.marginLeft = position + 'vw';
-            max = true;
-        }
-        jackBlack.style.backgroundImage = "url('./img/game/jCaminandoV2-original.webp')"
+        pressKeys[event.key] = true;
     }
     if (event.key == 'ArrowLeft') {
-        jackBlack.style.transform = 'scaleX(-1)';
-        if (cont > 0) {
-            position = position - margin;
-            if (position < limit && max == false) {
-                ground.style.marginLeft = '-' + position + 'vw';
-                // console.log("JACK LEFT: ", position);
-            }
-            if (max == true) {
-                jackBlack.style.marginLeft = position + 'vw';
-                // console.log("JACK LEF mas: ", position);
-                if (position < 0) {
-                    max = false;
-                    position = limit
-                }
-            }
-            jackBlack.style.backgroundImage = "url('./img/game/jCaminandoV2-original.webp')"
-            cont--
-        }
+        pressKeys[event.key] = true;
     }
     if (event.key == 'ArrowUp' && !event.repeat) {
-        upPress = true
-        setTimeout(() => {
-            upPress = false;
-        }, 500)
-        jackBlack.style.marginTop = "-160px"
-        setTimeout(() => {
-            jackBlack.style.marginTop = "0px"
-            // ground.style.transition = 'unset';
-        }, 650)
+        pressKeys[event.key] = true;
     }
-
     if (event.key == 'x') {
-        jackBlack.style.backgroundImage = "url('./img/game/Ataque-original.webp')"
+        pressKeys[event.key] = true;
     }
-
-    if (rightPress && upPress) {
-        position = position + 20;
-        if (max == false) {
-            cont = cont + 35
-            ground.style.marginLeft = '-' + position + 'vw';
-            console.log('false: ', cont);
-        } else {
-            // cont = cont + 3500
-            // position = limit
-            position = position - (limit)
-            jackBlack.style.marginLeft = '-' + position + 'vw';
-            console.log('true: ', cont);
-        }
-      
-        ground.style.transition = 'margin-left 1s';
-        rightPress = false;
-        upPress = false;
-        jackBlack.style.backgroundImage = "url('./img/game/jCaminandoV2-original.webp')"
+    if (keyPressed == 0) {
+        keyPress()
     }
-    setTimeout(() => {
-        ground.style.transition = 'margin-left 0s';
-    }, 750)
-
 })
+
+let jump = 26;
+let xd = false;
+
+function keyPress() {
+    keyPressed = 1;
+    console.log(xd);
+    interval = setInterval(() => {
+        console.log(cont);
+        if (cont > 115 && cont < 213) {
+            xd = true
+        } else if(cont < 115 && cont > 113 || cont > 213 && cont < 215) {
+            xd = false
+            jackBlack.style.marginTop = "0px"
+        }
+
+        if (pressKeys['ArrowRight']) {
+            jackBlack.style.transform = 'scaleX(1)';
+            jackBlack.style.backgroundImage = "url('./img/game/Caminando-original.webp')";
+
+            if (cont != 115 && cont < 281 || pressKeys['ArrowUp']) {
+                cont++
+                position = cont * margin;
+                if (position < limit) {
+                    // console.log("GR: ", position);
+                    ground.style.marginLeft = '-' + position + 'vw';
+                    // max = true;
+                } else {
+                    position = position - (limit)
+                    // console.log("JACK: ", position);
+                    jackBlack.style.marginLeft = position + 'vw';
+                    max = true;
+                }
+            }
+        }
+        if (pressKeys['ArrowLeft']) {
+            jackBlack.style.transform = 'scaleX(-1)';
+            jackBlack.style.backgroundImage = "url('./img/game/jCaminandoV2-original.webp')"
+            if (cont > 0 && cont != 213 || pressKeys['ArrowUp']) {
+                position = position - margin;
+                if (position < limit && max == false) {
+                    ground.style.marginLeft = '-' + position + 'vw';
+                    // console.log("JACK LEFT: ", position);
+                }
+                if (max == true) {
+                    jackBlack.style.marginLeft = position + 'vw';
+                    // console.log("JACK LEF mas: ", position);
+                    if (position < 0) {
+                        max = false;
+                        position = limit
+                    }
+                }
+                cont--
+            }
+        }
+        if (pressKeys['ArrowUp']) {
+            jackBlack.style.marginTop = "-" + jump + "vw"
+            setTimeout(() => {
+                if (xd == true) {
+                    jackBlack.style.marginTop = "-10vw"
+                } else {
+                    jackBlack.style.marginTop = "0px"
+                }
+            }, 650)
+        }
+
+        if (pressKeys['x']) {
+            jackBlack.style.backgroundImage = "url('./img/game/Ataque-original.webp')";
+        }
+    }, timePress)
+
+}
+
+window.addEventListener('keyup', function (event) {
+    delete pressKeys[event.key];//Eliminar tecla presionada del objeto
+    jackBlack.style.backgroundImage = "url('./img/game/Parada-original.webp')";
+    clearTimeout(timeSet);//Limpiar el set Time de Jack Descando
+    clearInterval(interval); //Limpiar que se sigan ejecutando letras
+
+    keyPress()
+
+    timeWaitJack(timeToSleep)
+})
+
 
 function timeWaitJack(timeWait) {
     timeSet = setTimeout(() => {
